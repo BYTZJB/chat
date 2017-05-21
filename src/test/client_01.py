51c6302b8f7923d0efd3c1bb0d13a20a8aecc9d9
wei yang <wei_yang@droidhang.com>
2017-05-21 19:06:52 +0800

    提交修改

--- src/test/client_01.py
+++ src/test/client_01.py
@@ -18,12 +18,12 @@ s.connect(('127.0.0.1', 5678))                       #建立连接
 
 def receive_message(conn):
     data = conn.recv(1024)
+    print("\n")
     print(data)
     receive_message(conn)
 
 t = threading.Thread(target=receive_message, args = (s,), name="Receive Message")
-print("客户端启动#####################")
-print("开始接受数据###################")
+print("客户端_1#####################")
 t.start()
 
 register = {'cmd':'1', 'username':'zst', 'password':'iyw'}
@@ -32,22 +32,18 @@ login = {'cmd':'2', 'password':'i'}
 while True:                                           #接受多次数据
 
     data = input('请输入要发送的数据：')                 #接收数据
-    print(data)
 
     if data == "0" or data == 0:
         break
 
     if data == "1" or data == 1:
-        print("cmd 1")
         s.send(json.dumps(register))
 
     if data == "2" or data == 2:
-        print("cmd 2")
         s.send(bytes(json.dumps(login)))
 
     if data == "3" or data == 3:
-        print("cmd 3")
-        message = input('请输入字符串')
+        message = input('请输入要发送的语句')
         chat_context = {'cmd': '3', 'id': '1', 'to_type': '1', 'to_id': '2', 'data': message}
         s.send(bytes(json.dumps(chat_context)))
 
